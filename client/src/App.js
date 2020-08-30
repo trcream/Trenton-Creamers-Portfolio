8360df3c17f0b87a575a03042326ae033e46eb64
Tcreamer <trentoncreamer@yahoo.com>
2020-08-30 10:50:36 -0700

    Installed Router and set up routes

--- client/src/App.js
+++ client/src/App.js
@@ -2,17 +2,25 @@
 import React, { Component } from "react";
 import "./App.css";
 import Header from "./components/Header";
-import TopNav from "./components/TopNav"
+import TopNav from "./components/TopNav";
+import {BrowserRouter as Router, Route} from "react-router-dom";
+import About from './pages/About'
+import Contact from "./pages/Contact"
 
-class App extends Component {
-  render() {
+
+function App() {
+  //render() 
     return (
-     <div>
-       <Header/>
-       <TopNav/>
-     </div>
+      <Router>
+        <div>
+          <Header />
+          <TopNav />
+          <Route exact path="/" component={About} />
+          <Route exact path="/contact" component={Contact} />
+        </div>
+      </Router>
     );
-  }
+  
 }
 
 export default App;
